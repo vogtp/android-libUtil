c1b4dabb7bb052a86894696ce4f2e1add3a04bcb
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-11-30 22:02:02 +0100

    initial

---
+++ src/ch/almana/android/logdebug/Debug.java
@@ -0,0 +1,24 @@
+package ch.almana.android.logdebug;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class Debug {
+
+	private static final int DEBUG_SIGNATURE_HASH = -1623526495;
+
+	public static boolean isUnsinedPackage(Activity act) {
+		String packageName = act.getApplication().getPackageName();
+		try {
+			PackageInfo packageInfo = act.getApplication().getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+			int hash = packageInfo.signatures[0].hashCode();
+			if (hash == DEBUG_SIGNATURE_HASH) {
+				return true;
+			}
+		} catch (NameNotFoundException e) {
+		}
+		return false;
+	}
+}
