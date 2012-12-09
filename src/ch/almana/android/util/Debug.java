eac4809906a599b7f011071292a993d7f3ed983f
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-12-10 00:10:49 +0100

    use context not activity

--- src/ch/almana/android/util/Debug.java
+++ src/ch/almana/android/util/Debug.java
@@ -1,6 +1,6 @@
 package ch.almana.android.util;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -9,10 +9,10 @@ public class Debug {
 
 	private static final int DEBUG_SIGNATURE_HASH = -1623526495;
 
-	public static boolean isUnsinedPackage(Activity act) {
-		String packageName = act.getApplication().getPackageName();
+	public static boolean isUnsinedPackage(Context ctx) {
+		String packageName = ctx.getApplicationInfo().packageName;
 		try {
-			PackageInfo packageInfo = act.getApplication().getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+			PackageInfo packageInfo = ctx.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
 			int hash = packageInfo.signatures[0].hashCode();
 			if (hash == DEBUG_SIGNATURE_HASH) {
 				return true;
